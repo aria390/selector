5518d15bb3234c93dee6d2c91d24cdfb02642b35
aria_bz <ariabahmanzangi@gmail.com>
2025-09-22 16:08:52 +0330

    v2

--- script.js
+++ script.js
@@ -5,23 +5,29 @@ const youTubeEl = document.getElementById("YouTube");
 const twitterEl = document.getElementById("Twitter");
 const linkEl = document.getElementById("Linkedin");
 const angelEl = document.getElementById("angel");
+const angelRotateEl = document.getElementById("angelrotate");
 
 angelEl.addEventListener("click", () => {
   selectEL.classList.toggle("hiddenul");
+  angelRotateEl.classList.toggle("rotate");
 });
 faceBookEL.addEventListener("click", () => {
   selectEL.classList.add("hiddenul");
   inputEl.innerHTML = faceBookEL.innerHTML;
+  angelRotateEl.classList.toggle("rotate");
 });
 youTubeEl.addEventListener("click", () => {
   selectEL.classList.add("hiddenul");
   inputEl.innerHTML = youTubeEl.innerHTML;
+  angelRotateEl.classList.toggle("rotate");
 });
 twitterEl.addEventListener("click", () => {
   selectEL.classList.add("hiddenul");
   inputEl.innerHTML = twitterEl.innerHTML;
+  angelRotateEl.classList.toggle("rotate");
 });
 linkEl.addEventListener("click", () => {
   selectEL.classList.add("hiddenul");
   inputEl.innerHTML = linkEl.innerHTML;
+  angelRotateEl.classList.toggle("rotate");
 });
